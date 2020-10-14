794f33e2929bbb7376fdd4bf53e1db732e3aac92
BenkeBenkeBenke <one2onesmile@gmail.com>
2020-10-14 15:56:41 +0200

    Level 2 + camera script added

---
+++ Assets/Scripts/CameraMovment.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject bg_House;
+    public GameObject bg_Sky;
+
+    public float smoothTime = 0.5F;
+    public Vector3 offset = new Vector3(0f, 1f, -10f);
+    private Vector3 _velocity;
+    private Transform _transform;
+
+    public bool sliding_BG;
+
+
+    void Start()
+    {
+        _transform = transform;
+        _transform.position = Player.transform.position + offset;
+    }
+
+    void Update()
+    {
+        Vector3 PlayerMovmentTarget = new Vector3(Input.GetAxis("Horizontal") * 2, 0,0);
+        Vector3 targetPosition = Player.transform.TransformPoint(offset + PlayerMovmentTarget);       
+        _transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref _velocity, smoothTime);
+
+        if(sliding_BG)
+        {
+            bg_House.transform.position = new Vector3(bg_House.transform.position.x - Input.GetAxis("Horizontal") / 2000, bg_House.transform.position.y, bg_House.transform.position.z);
+            bg_Sky.transform.position = new Vector3(bg_Sky.transform.position.x - Input.GetAxis("Horizontal") / 5000, bg_Sky.transform.position.y, bg_Sky.transform.position.z);
+        }
+
+    }
+}
