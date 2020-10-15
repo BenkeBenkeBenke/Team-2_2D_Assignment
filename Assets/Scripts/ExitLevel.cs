2e2024c79caaa5ff0ce28e99ff93364bf8f6e1a7
BenkeBenkeBenke <one2onesmile@gmail.com>
2020-10-15 11:20:34 +0200

    Exit/Enter next scene and spawning added to Level 1 and 2.

--- Assets/Scripts/ExitLevel.cs
+++ Assets/Scripts/ExitLevel.cs
@@ -6,11 +6,20 @@ using UnityEngine.SceneManagement;
 public class ExitLevel : MonoBehaviour
 {
     public Object SceneToLoad;
+    public Vector3 PlaceToSpawnPlayer;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            
+            //Save current Scene so when you load next scene you start in the correct place.
+            GameObject[] Player = GameObject.FindGameObjectsWithTag("Player");
+            PlayerStats PlayerStatsScript = Player[0].GetComponent<PlayerStats>();
+           // PlayerStatsScript.LastScene = SceneManager.GetActiveScene();
+           // PlayerStatsScript.LastSceneString = PlayerStatsScript.LastScene.name;
+            PlayerStatsScript.PlaceToSpawnPlayerOnLoad = PlaceToSpawnPlayer;
+
             SceneManager.LoadScene(SceneToLoad.name);
         }
     }
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerStats : MonoBehaviour
+{
+    public Scene LastScene;
+    public string LastSceneString;
+    public Vector3 PlaceToSpawnPlayerOnLoad;
+
+    void Awake()
+    {
+        // To make player consistent.
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Player");
+
+        if (objs.Length > 1)
+        {
+            // if player enter a new room
+
+            Destroy(this.gameObject);
+        }
+        objs[0].transform.position = objs[0].GetComponent<PlayerStats>().PlaceToSpawnPlayerOnLoad;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
