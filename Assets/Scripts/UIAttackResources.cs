ed8411db8b8ddfdd9157490e2700f80feeacffaf
Jaller <Jaller.ole@gmail.com>
2020-10-16 09:16:12 +0200

    Player resource bars
    Player resource bars prefab added

---
+++ Assets/Scripts/UIAttackResources.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAttackResources : MonoBehaviour
+{
+    public Slider redSlider;
+    public Slider blueSlider;
+    
+    private PlayerAttackResource _attackResource;
+
+    private void Start()
+    {
+        _attackResource = GameObject.Find("Player").GetComponent<PlayerAttackResource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        redSlider.value = _attackResource.redResource;
+        blueSlider.value = _attackResource.blueResource;
+    }
+}
