917567e56e09d83f2015c34bdb520221ff90eb85
BenkeBenkeBenke <one2onesmile@gmail.com>
2020-10-21 10:00:00 +0200

    Fire and ice enemy death animation added

---
+++ Assets/Scripts/DestroyGO.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyGO : MonoBehaviour
+{
+    [Header("Behavior")]
+    public bool destroyAfterTimer;
+
+    [Header("Timer Setting")]
+    public float DaetroyAfter = 60;
+
+    void Update()
+    {
+        if (destroyAfterTimer)
+        {
+            DaetroyAfter--;
+            if (DaetroyAfter < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
