b0715f2fed2bb94327dbc35c7f1e95ad1f35a51a
Jaller <Jaller.ole@gmail.com>
2020-10-22 15:56:35 +0200

    Update PlayerRespawn.cs
    Changed respawn to reload level and set start position and checkpoint so enemies will respawn on death

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -1,27 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class PlayerRespawn : MonoBehaviour
 {
     public Vector3 respawnLocation;
+    private PlayerStats _playerStats;
 
     // Start is called before the first frame update
     void Start()
     {
-        respawnLocation = gameObject.transform.position;
+        //respawnLocation = gameObject.transform.position;
+        _playerStats = gameObject.GetComponent<PlayerStats>();
     }
 
     public void SetActiveCheckpoint(GameObject newCheckpoint)
     {
         respawnLocation = newCheckpoint.transform.position;
+        _playerStats.PlaceToSpawnPlayerOnLoad = newCheckpoint.transform.position;
         newCheckpoint.transform.Find("Particles").GetComponent<ParticleSystem>().Play();
 
     }
     public void RespawnPlayer()
     {
-        gameObject.transform.position = respawnLocation;
+        var currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+        
         gameObject.GetComponent<PlayerHealth>().health = 10;
+        //gameObject.transform.position = respawnLocation;
     }
 }
