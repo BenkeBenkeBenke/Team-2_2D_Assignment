a7c31b18d1271cd1f8c0bd607960bd4733f0e906
Jaller <Jaller.ole@gmail.com>
2020-10-23 14:33:29 +0200

    shield particles
    SHield particles added for eneies. Shield velocity added to player

---
+++ Assets/Scripts/EnemyAI/EnemyShieldColor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShieldColor : MonoBehaviour
+{
+    private EnemyAI _enemyAI;
+    private ParticleSystem _particleSystem;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyAI = transform.GetComponentInParent<EnemyAI>();
+        _particleSystem = GetComponent<ParticleSystem>();
+        
+        var main = _particleSystem.main;
+
+        if (_enemyAI.redShield)
+        {
+            main.startColor = new Color(255, 0, 0, 1);
+        }
+
+        if (_enemyAI.blueShield)
+        {
+            main.startColor = new Color(0, 0, 255, 1);
+        }
+
+        if (!_enemyAI.redShield && !_enemyAI.blueShield)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
